5d1bc339fac7580dbd254d87b199cc58fcee7808
GinMu <gin.musheng@gmail.com>
2020-02-14 16:57:54 +0800

    config of SEAAPS chain

--- src/wallet_config.js
+++ src/wallet_config.js
@@ -2,21 +2,27 @@
  * Created by yalei on 2017/4/25.
  */
 const config = [{
-    currency: 'SWT',
-    ACCOUNT_ALPHABET: 'jpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65rkm8oFqi1tuvAxyz',
-    SEED_PREFIX: 33,
-    ACCOUNT_PREFIX: 0,
-    ACCOUNT_ZERO: 'jjjjjjjjjjjjjjjjjjjjjhoLvTp',
-    ACCOUNT_ONE: 'jjjjjjjjjjjjjjjjjjjjBZbvri',
-    fee: 10
+  currency: 'SWT',
+  ACCOUNT_ALPHABET: 'jpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65rkm8oFqi1tuvAxyz',
+  SEED_PREFIX: 33,
+  ACCOUNT_PREFIX: 0,
+  ACCOUNT_ZERO: 'jjjjjjjjjjjjjjjjjjjjjhoLvTp',
+  ACCOUNT_ONE: 'jjjjjjjjjjjjjjjjjjjjBZbvri',
+  fee: 10
 }, {
-    currency: 'BWT',
-    ACCOUNT_ALPHABET: 'bpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2jcdeCg65rkm8oFqi1tuvAxyz',
-    SEED_PREFIX: 33,
-    ACCOUNT_PREFIX: 0,
-    ACCOUNT_ZERO: 'bbbbbbbbbbbbbbbbbbbbbhoLvTp',
-    ACCOUNT_ONE: 'bbbbbbbbbbbbbbbbbbbbBZjvri',
-    fee: 10
+  currency: 'BWT',
+  ACCOUNT_ALPHABET: 'bpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2jcdeCg65rkm8oFqi1tuvAxyz',
+  SEED_PREFIX: 33,
+  ACCOUNT_PREFIX: 0,
+  ACCOUNT_ZERO: 'bbbbbbbbbbbbbbbbbbbbbhoLvTp',
+  ACCOUNT_ONE: 'bbbbbbbbbbbbbbbbbbbbBZjvri',
+  fee: 10
+}, {
+  currency: 'SEAA',
+  ACCOUNT_ALPHABET: 'dpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcjeCg65rkm8oFqi1tuvAxyz',
+  SEED_PREFIX: 33,
+  ACCOUNT_PREFIX: 0,
+  fee: 100000
 }];
 
 module.exports = config;
