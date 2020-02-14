d38810591ec28537288cac27df5f17609ed93768
GinMu <gin.musheng@gmail.com>
2020-02-14 17:53:54 +0800

    fixed "@babel/runtime" not be found

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,34 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const babel = require('gulp-babel');
 
-gulp.task('eslint', function () {
-    return gulp.src(['src/**/*.js'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task('eslint', function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('build', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel({
-            presets: [
-                ["@babel/preset-env", {
-                    "targets": {
-                        "browsers": ["last 2 versions"],
-                        "node": "8.11.3"
-                    }
-                }]
-            ],
-            plugins: [
-                "@babel/plugin-transform-runtime"
-            ]
-        }))
-        .pipe(gulp.dest('lib/'))
+gulp.task('build', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(babel({
+      presets: [
+        ["@babel/preset-env", {
+          "targets": {
+            "browsers": ["last 2 versions"],
+            "node": "8.11.3"
+          }
+        }]
+      ],
+      plugins: [
+        "@babel/plugin-transform-runtime"
+      ]
+    }))
+    .pipe(gulp.dest('lib/'))
 })
 
-gulp.task('watch', function () {
-    gulp.watch(['src/**/*.js'], ['eslint']);
+gulp.task('watch', function() {
+  gulp.watch(['src/**/*.js'], ['eslint']);
 })
 
-gulp.task('default', gulp.parallel('eslint'));
+gulp.task('default', gulp.parallel('build', 'eslint'));
 gulp.task('dev', gulp.parallel('watch', 'eslint'));
--- test/test_keypairs.js
+++ test/test_keypairs.js
@@ -1,41 +1,41 @@
-const Keypairs = require('../src/keypairs');
+const Keypairs = require('../lib/keypairs');
 const expect = require('chai').expect;
 const {
-    chains,
-    data
+  chains,
+  data
 } = require('./config');
 
-describe('test keypairs', function () {
+describe('test keypairs', function() {
 
-    describe('create instance', function () {
-        it('throw error if the config of given token is empty', function () {
-            expect(() => new Keypairs('moac')).throw('config of moac is empty')
-        })
+  describe('create instance', function() {
+    it('throw error if the config of given token is empty', function() {
+      expect(() => new Keypairs('moac')).throw('config of moac is empty')
+    })
+  })
+
+  describe('convertAddressToBytes and convertBytesToAddress', function() {
+    it('convert address to bytes successfully', function() {
+      for (let chain of chains) {
+        let validAddress = data[chain].validAddress;
+        let inst = new Keypairs(chain == 'swt' ? undefined : chain);
+        let bytes = inst.convertAddressToBytes(validAddress);
+        let address = inst.convertBytesToAddress(Buffer.from(bytes));
+        expect(address).to.equal(validAddress)
+      }
     })
 
-    describe('convertAddressToBytes and convertBytesToAddress', function () {
-        it('convert address to bytes successfully', function () {
-            for (let chain of chains) {
-                let validAddress = data[chain].validAddress;
-                let inst = new Keypairs(chain == 'swt' ? undefined : chain);
-                let bytes = inst.convertAddressToBytes(validAddress);
-                let address = inst.convertBytesToAddress(Buffer.from(bytes));
-                expect(address).to.equal(validAddress)
-            }
-        })
-
-        it('convertAddressToBytes in error if the address is invalid', function () {
-            for (let chain of chains) {
-                let inst = new Keypairs(chain == 'swt' ? undefined : chain);
-                expect(() => inst.convertAddressToBytes(undefined)).throw('convert address to bytes in error')
-            }
-        })
-
-        it('convertBytesToAddress in error if bytes is invalid', function () {
-            for (let chain of chains) {
-                let inst = new Keypairs(chain == 'swt' ? undefined : chain);
-                expect(() => inst.convertBytesToAddress('')).throw('convert bytes to address in error')
-            }
-        })
+    it('convertAddressToBytes in error if the address is invalid', function() {
+      for (let chain of chains) {
+        let inst = new Keypairs(chain == 'swt' ? undefined : chain);
+        expect(() => inst.convertAddressToBytes(undefined)).throw('convert address to bytes in error')
+      }
     })
+
+    it('convertBytesToAddress in error if bytes is invalid', function() {
+      for (let chain of chains) {
+        let inst = new Keypairs(chain == 'swt' ? undefined : chain);
+        expect(() => inst.convertBytesToAddress('')).throw('convert bytes to address in error')
+      }
+    })
+  })
 })
